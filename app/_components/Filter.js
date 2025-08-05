0fad2eefc679f142aebeecb7c240c3a4e95813f6
EphiatEneyew <ephremeneyewsisay@gmail.com>
2025-08-05 16:10:39 -0700

    add guests on the button

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -17,9 +17,9 @@ export default function Filter() {
   return (
     <div className="border border-primary-800 flex">
         <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "all" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("all")}>All cabins</button>
-        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "small" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("small")}>1&mdash;3</button>
-        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "medium" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("medium")}>4&mdash;7</button>
-        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "large" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("large")}>8&mdash;12</button>
+        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "small" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("small")}>1&mdash;3 guests</button>
+        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "medium" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("medium")}>4&mdash;7 guests</button>
+        <button className={`px-5 py-2 hover:bg-primary-700 ${activeFilter === "large" ? 'bg-primary-700 text-primary-50' : " "}`} onClick={() => handleFilter("large")}>8&mdash;12 guests</button>
     </div>
   );
 }
